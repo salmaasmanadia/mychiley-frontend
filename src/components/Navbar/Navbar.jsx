99b100949abd399d07e402dc87410bf2a7cfbf2f
salmaasmanadia <salma.asma.nadia@mail.ugm.ac.id>
2024-04-02 13:03:12 +0700

    feat : add sign up button to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -181,6 +181,15 @@ const Navbar = () => {
                         </Link>
                         </div>
                     </li>
+                    <li className="text-black">
+                        <div className="flex">
+                        <Link href="/signup">
+                            <div className="font-poppins font-semibold bg-purple-800 hover:bg-white hover:text-purple-800 border-2 hover:border-purple-800 py-1 px-3 rounded-xl text-white mr-2">
+                            Sign-Up
+                            </div>
+                        </Link>
+                        </div>
+                    </li>
                     </ul>
                 </div>
                 </div>
