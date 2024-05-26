ea365694d8d1fd6ddf58e47ea6b138ed504693c1
salmaasmanadia <salma.asma.nadia@mail.ugm.ac.id>
2024-05-26 19:05:32 +0700

    fix: history page

--- src/components/Card/HistoryCard.jsx
+++ src/components/Card/HistoryCard.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const HistoryCard = (e) => {
     return (
     <>
-        <div className="flex justify-between md:px-8 px-4 bg-[#FAFAFA] shadow-xl rounded-lg my-2">
+        <div className="flex justify-between md:px-8 px-4 bg-purple-200 shadow-xl rounded-lg my-2">
         <div className="max-w-[200px] md:max-w-100 ">
             <Image
             className="rounded-lg md:max-w-[200px] max-w-[120px]"
@@ -14,7 +14,7 @@ const HistoryCard = (e) => {
             height={200}
             />
         </div>
-        <div className="w-fit overflow-hidden ml-3">
+        <div className="w-fit h-fit overflow-hidden ml-3 mt-5">
             <div className="block text-black md:text-lg text-sm">Prediction = {e.pred}</div>
             <div className="block text-black md:text-lg text-sm">Email = {e.email}</div>
             <div className="block text-black md:text-lg text-sm">Date = {e.date}</div>
--- src/pages/history.js
+++ src/pages/history.js
@@ -6,15 +6,15 @@ const history = () => {
     return (
         <>
         <DefaultLayout>
-            <div className="flex w-full h-screen historyPage-bg">
+            <div className="flex w-full h-100vh historyPage-bg">
             <div className="w-full">
                 <div className="flex w-screen pb-12 justify-center mt-32 mx-8">
-                <h1 className="text-center font-semibold font-serif-inter text-6xl text-purple-500">
+                <h1 className="text-center font-semibold font-poppins text-6xl text-purple-900">
                     Prediction History
                 </h1>
                 </div>
                 <div className="flex justify-center">
-                <div className=" bg-white rounded-xl w-3/4 h-auto">
+                <div className=" rounded-xl w-1.5/4 h-auto">
                     <HistoryContainer/>
                 </div>
                 </div>
